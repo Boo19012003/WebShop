0d7be121b803a625e4130ed84554797c589f3f51
Boo <boo19012003@gmail.com>
2024-06-06 08:21:23 +0700

    thêm các tệp mới cho dự án

--- fe/celeryshop/src/index.js
+++ fe/celeryshop/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import HomePage from './pages/admin/users/homePage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1> CELERY-LG</h1>);
+root.render(<HomePage />);
